1fdb69bee928ba19348c88fd7b8da43809729b3c
= <=>
2020-07-25 20:30:12 +0800

    version:2

--- src/main/java/com/gznc/Demo02Application.java
+++ src/main/java/com/gznc/Demo02Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Demo02Application {
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer(){
+        return new TomcatServletWebServerFactory(80) ;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo02Application.class, args);
